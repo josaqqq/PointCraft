70b71d1b7c028dc3358f734e4b1ee084d0934c56
sakajo6 <sakajoy9@gmail.com>
2023-10-24 23:55:21 +0900

    Added glm library

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,7 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
-#include <Eigen/Dense>
-
-#include "../include/linmath.h"
+#include <glm/glm.hpp>
+#include <glm/ext.hpp>
 
 #include <iostream>
 
@@ -16,19 +15,22 @@ struct Vertex {
     float x, y;
     float r, g, b;
 };
-const Vertex vertices[3] = {
-    {-0.6, -0.4, 1.0, 0.0, 0.0},
-    {0.6, -0.4, 0.0, 1.0, 0.0},
-    {0.0, 0.6, 0.0, 0.0, 1.0},
+const int VertexSiz = 5;
+const Vertex vertices[VertexSiz] = {
+    {-0.6, -0.4, 1.0, 1.0, 1.0},
+    {0.6, -0.4, 1.0, 1.0, 1.0},
+    {0.0, 0.6, 1.0, 1.0, 1.0},
+    {1.0, 0.8, 1.0, 1.0, 1.0},
+    {1.0, -0.8, 1.0, 1.0, 1.0}
 };
 
 static const char* vertex_shader_text =
 "#version 110\n"
-"uniform mat4 MVP;\n"
-"attribute vec3 vCol;\n"
-"attribute vec2 vPos;\n"
-"varying vec3 color;\n"
-"void main()\n"
+"uniform mat4 MVP;\n"       // ModelViewProjection
+"attribute vec2 vPos;\n"    // Vertex position
+"attribute vec3 vCol;\n"    // Color information
+"varying vec3 color;\n"     // Passed to fragment_shader
+"void main()\n"             // Executed for each vertex
 "{\n"
 "    gl_Position = MVP * vec4(vPos, 0.0, 1.0);\n"
 "    color = vCol;\n"
@@ -53,7 +55,8 @@ int main() {
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
 
-    GLFWwindow* window = glfwCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "PointCraft", NULL, NULL); // window and monitor are not specified
+    GLFWwindow* window = glfwCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "PointCraft", NULL, NULL);
+        // window and monitor are not specified
     if (!window) {
         std::cerr << "Failed to create window." << std::endl;
         glfwTerminate();
@@ -70,37 +73,42 @@ int main() {
         glfwTerminate();
         return -1;
     }
-    glfwSwapInterval(1);
 
-    // Shader information
+    // Buffer & Shader & Program
     GLuint vertex_buffer, vertex_shader, fragment_shader, program;
     GLint mvp_location, vpos_location, vcol_location;
 
+    // Register vertices to GL_ARRAY_BUFFER
     glGenBuffers(1, &vertex_buffer);
     glBindBuffer(GL_ARRAY_BUFFER, vertex_buffer);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
- 
+        // GL_STATIC_DRAW: The data store contents will be modified once and used many times as the source for GL drawing commands.
+        // GL_DYNAMIC_DRAW: The data store contents will be modified repeatedly and used many times as the source for GL drawing commands.
+    
+    // Register vertex_shader_text to GL_VERTEX_SHADER
     vertex_shader = glCreateShader(GL_VERTEX_SHADER);
     glShaderSource(vertex_shader, 1, &vertex_shader_text, NULL);
     glCompileShader(vertex_shader);
- 
+    
+    // Register fragment_shader_text to GL_FRAGMENT_SHADER
     fragment_shader = glCreateShader(GL_FRAGMENT_SHADER);
     glShaderSource(fragment_shader, 1, &fragment_shader_text, NULL);
     glCompileShader(fragment_shader);
  
+    // Register program
     program = glCreateProgram();
     glAttachShader(program, vertex_shader);
     glAttachShader(program, fragment_shader);
     glLinkProgram(program);
- 
-    mvp_location = glGetUniformLocation(program, "MVP");
-    vpos_location = glGetAttribLocation(program, "vPos");
-    vcol_location = glGetAttribLocation(program, "vCol");
- 
+    
+    // Set the variables in shader_text
+    mvp_location = glGetUniformLocation(program, "MVP");    // ModelViewProjection
+    vpos_location = glGetAttribLocation(program, "vPos");   // (x, y)
+    vcol_location = glGetAttribLocation(program, "vCol");   // (r, g, b)
     glEnableVertexAttribArray(vpos_location);
+    glEnableVertexAttribArray(vcol_location);
     glVertexAttribPointer(vpos_location, 2, GL_FLOAT, GL_FALSE,
                           sizeof(vertices[0]), (void*) 0);
-    glEnableVertexAttribArray(vcol_location);
     glVertexAttribPointer(vcol_location, 3, GL_FLOAT, GL_FALSE,
                           sizeof(vertices[0]), (void*) (sizeof(float) * 2));
 
@@ -108,22 +116,35 @@ int main() {
     while (!glfwWindowShouldClose(window)) {
         float ratio;
         int width, height;
-        mat4x4 m, p, mvp;
+        glm::mat4 m, v, p, mvp;
  
         glfwGetFramebufferSize(window, &width, &height);
         ratio = width / (float) height;
  
         glViewport(0, 0, width, height);
         glClear(GL_COLOR_BUFFER_BIT);
- 
-        mat4x4_identity(m);
-        mat4x4_rotate_Z(m, m, (float) glfwGetTime());
-        mat4x4_ortho(p, -ratio, ratio, -1.f, 1.f, 1.f, -1.f);
-        mat4x4_mul(mvp, p, m);
- 
+
+        // Projection
+        p = glm::perspective(45.0f, ratio, 0.1f, 100.0f);
+            // fovy(deg):   Degree of zoom (30 - 90)
+            // aspect(w/h): Aspect ratio
+            // znear:       Near clipping plane
+            // zfar:        Far clipping plane
+        // View
+        v = glm::lookAt(
+            glm::vec3(0.0, 0.0, -3.0), // Camera position
+            glm::vec3(0.0, 0.0, 0.0),   // Viewing position
+            glm::vec3(0.0, 1.0, 0.0)    // Upward direction of Camera
+        );  
+        // Model
+        m = glm::mat4(1.0f);
+        m = glm::rotate(m, (float)glfwGetTime(), glm::vec3(0.f, 0.f, 1.f)); // Rotate Model with z-axis
+        mvp = p * v * m;
+    
+        // Select the used program
         glUseProgram(program);
-        glUniformMatrix4fv(mvp_location, 1, GL_FALSE, (const GLfloat*) mvp);
-        glDrawArrays(GL_TRIANGLES, 0, 3);
+        glUniformMatrix4fv(mvp_location, 1, GL_FALSE, glm::value_ptr(mvp));
+        glDrawArrays(GL_POINTS, 0, VertexSiz);
 
         glfwSwapBuffers(window);
         glfwPollEvents();
