1769b90a62b472fee4015cd1c0d7a23dee83031f
sakajo6 <sakajoy9@gmail.com>
2023-10-25 14:13:18 +0900

    Implemented sketch function

--- src/main.cpp
+++ src/main.cpp
@@ -4,22 +4,42 @@
 #include <glm/ext.hpp>
 
 #include <iostream>
+#include <vector>
 
 const unsigned int WINDOW_WIDTH = 1280;
 const unsigned int WINDOW_HEIGHT = 720;
+const float GRID_SIZE= 0.02f;
 
+// Modle View Projection values
+glm::mat4 M;    // Model matrix
+float Fovy = 45.0f;                                  // Degree of zoom (deg)
+float Znear = 3.0f;                                  // Near clipping plane
+float Zfar = 100.0f;                                 // Far clipping plane
+
+glm::mat4 V;    // View matrix
+glm::vec3 Camera = glm::vec3(0.0f, 0.0f, 3.0f);    // Camera position
+glm::vec3 View = glm::vec3(0.0f, 0.0f, 0.0f);       // Viewing position
+glm::vec3 Upward = glm::vec3(0.0f, 1.0f, 0.0f);     // Upward direction of Camera
+
+glm::mat4 P;    // Projetion matrix
+glm::mat4 mvp;  // Model View Projection matrix
+
+// Callback
 void key_callback(GLFWwindow*, int, int, int, int);
 void error_callback(int, const char*);
 void cursor_position_callback(GLFWwindow*, double, double);
 void mouse_button_callback(GLFWwindow*, int, int, int);
 
 struct Vertex {
-    float x, y;
-    float r, g, b;
+    float x, y;     // TODO: 2D -> 3D
+    float r, g, b;  // TODO: send color information to shader with glUniform4f https://stackoverflow.com/questions/54583368/how-to-draw-multiple-objects-in-opengl-using-multiple-vao-and-vbo
 };
+const int VertexMaxSiz = 10000;
 const int VertexSiz = 2500;
 const int VertexEdge = 50;
-Vertex vertices[VertexSiz];
+int cur_vertexsiz = 2500;
+std::vector<Vertex> vertices;
+std::vector<Vertex> vertices_2;
 
 static const char* vertex_shader_text =
 "#version 110\n"
@@ -43,11 +63,24 @@ static const char* fragment_shader_text =
 
 int main() {
     // Vertex initialization
+    vertices.resize(VertexMaxSiz);
     for (int i = 0; i < VertexSiz; i++) {
-        float x = i/VertexEdge, y = i%VertexEdge;
+        float x = i%VertexEdge, y = i/VertexEdge;
         vertices[i] = Vertex {
-            x: 0.02f * x - 0.5f,
-            y: 0.02f * y - 0.5f,
+            x: GRID_SIZE * x - GRID_SIZE * VertexEdge * 0.5f,
+            y: GRID_SIZE * y - GRID_SIZE * VertexEdge * 0.5f,
+            r: 1.0f,
+            g: 1.0f,
+            b: 1.0f
+        };
+    }
+
+    vertices_2.resize(VertexMaxSiz);
+    for (int i = 0; i < VertexSiz; i++) {
+        float x = i%VertexEdge, y = i/VertexEdge;
+        vertices_2[i] = Vertex {
+            x: GRID_SIZE * x,
+            y: GRID_SIZE * y,
             r: 1.0f,
             g: 1.0f,
             b: 1.0f
@@ -86,15 +119,19 @@ int main() {
     }
 
     // Buffer & Shader & Program
-    GLuint vertex_buffer, vertex_shader, fragment_shader, program;
+    GLuint vertex_buffer, vertex_buffer_2, vertex_shader, fragment_shader, program;
     GLint mvp_location, vpos_location, vcol_location;
 
     // Register vertices to GL_ARRAY_BUFFER
     glGenBuffers(1, &vertex_buffer);
     glBindBuffer(GL_ARRAY_BUFFER, vertex_buffer);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(Vertex) * vertices.size(), vertices.data(), GL_DYNAMIC_DRAW);
         // GL_STATIC_DRAW: The data store contents will be modified once and used many times as the source for GL drawing commands.
-        // GL_DYNAMIC_DRAW: The data store contents will be modified repeatedly and used many times as the source for GL drawing commands.
+        // GL_DYNAMIC_DRAW: The data store contents will be modified repeatedly and used many times as the source for GL drawing commands. 
+    glGenBuffers(1, &vertex_buffer_2);
+    glBindBuffer(GL_ARRAY_BUFFER, vertex_buffer_2);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(Vertex) * vertices_2.size(), vertices_2.data(), GL_DYNAMIC_DRAW);
+
     
     // Register vertex_shader_text to GL_VERTEX_SHADER
     vertex_shader = glCreateShader(GL_VERTEX_SHADER);
@@ -118,44 +155,39 @@ int main() {
     vcol_location = glGetAttribLocation(program, "vCol");   // (r, g, b)
     glEnableVertexAttribArray(vpos_location);
     glEnableVertexAttribArray(vcol_location);
-    glVertexAttribPointer(vpos_location, 2, GL_FLOAT, GL_FALSE,
-                          sizeof(vertices[0]), (void*) 0);
-    glVertexAttribPointer(vcol_location, 3, GL_FLOAT, GL_FALSE,
-                          sizeof(vertices[0]), (void*) (sizeof(float) * 2));
 
     // Display window
     while (!glfwWindowShouldClose(window)) {
-        float ratio;
+        float aspect;
         int width, height;
-        glm::mat4 M, V, P, mvp;
  
         glfwGetFramebufferSize(window, &width, &height);
-        ratio = width / (float) height;
+        aspect = width / (float) height;
  
         glViewport(0, 0, width, height);
         glClear(GL_COLOR_BUFFER_BIT);
 
-        // Projection
-        P = glm::perspective(45.0f, ratio, 0.1f, 100.0f);
-            // fovy(deg):   Degree of zoom (30 - 90)
-            // aspect(w/h): Aspect ratio
-            // znear:       Near clipping plane
-            // zfar:        Far clipping plane
-        // View
-        V = glm::lookAt(
-            glm::vec3(0.0, 0.0, -3.0),  // Camera position
-            glm::vec3(0.0, 0.0, 0.0),   // Viewing position
-            glm::vec3(0.0, 1.0, 0.0)    // Upward direction of Camera
-        );  
-        // Model
-        M = glm::mat4(1.0f);
+        P = glm::perspective(Fovy, aspect, Znear, Zfar);    // Projection
+        V = glm::lookAt(Camera, View, Upward);              // View
+        M = glm::mat4(1.0f);                                // Model
         // M = glm::rotate(M, (float)glfwGetTime(), glm::vec3(0.f, 0.f, 1.f)); // Rotate Model with z-axis
         mvp = P * V * M;
 
         // Execute program
         glUseProgram(program);
         glUniformMatrix4fv(mvp_location, 1, GL_FALSE, glm::value_ptr(mvp));
-        glDrawArrays(GL_POINTS, 0, VertexSiz);
+
+        // Bind vertex_buffer
+        glBindBuffer(GL_ARRAY_BUFFER, vertex_buffer);
+        glVertexAttribPointer(vpos_location, 2, GL_FLOAT, GL_FALSE, sizeof(vertices[0]), (void*) 0);
+        glVertexAttribPointer(vcol_location, 3, GL_FLOAT, GL_FALSE, sizeof(vertices[0]), (void*) (sizeof(float) * 2));
+        glDrawArrays(GL_POINTS, 0, cur_vertexsiz);
+
+        // Draw vertex_buffer_2
+        // glBindBuffer(GL_ARRAY_BUFFER, vertex_buffer_2);
+        // glVertexAttribPointer(vpos_location, 2, GL_FLOAT, GL_FALSE, sizeof(vertices_2[0]), (void*) 0);
+        // glVertexAttribPointer(vcol_location, 3, GL_FLOAT, GL_FALSE, sizeof(vertices_2[0]), (void*) (sizeof(float) * 2));
+        // glDrawArrays(GL_POINTS, 0, cur_vertexsiz);
 
         glfwSwapBuffers(window);
         // glfwPollEvents()    // Processes only received events and then returns immediately
@@ -173,6 +205,11 @@ void key_callback(GLFWwindow *window, int key, int scannode, int action, int mod
                 glfwSetWindowShouldClose(window, GLFW_TRUE);
             }
             break;
+        case GLFW_KEY_Z:
+            if (action == GLFW_PRESS) {
+                cur_vertexsiz = VertexSiz;
+            }
+            break;
     }
 }
 
@@ -180,13 +217,41 @@ void error_callback(int error, const char* description) {
     fprintf(stderr, "Error: %s\n", description);
 }
 
+bool Dragging = false;
 void cursor_position_callback(GLFWwindow *window, double xpos, double ypos) {
-    std::cout << xpos << " " << ypos << std::endl;
+    if (Dragging) {
+        int width, height;
+        glfwGetFramebufferSize(window, &width, &height);
+        glm::vec3 wolrd_pos = glm::unProject(
+            glm::vec3((float)xpos, (float)height - (float)ypos, 0.0f),  // 0.0(Znear) - 1.0(Zfar)
+            M,
+            P,
+            glm::vec4(0.0f, 0.0f, (float)width, (float)height)
+        );
+        vertices[cur_vertexsiz] = Vertex {
+            x: wolrd_pos.x,
+            y: wolrd_pos.y,
+            r: 1.0f,
+            g: 1.0f,
+            b: 1.0f
+        };
+        cur_vertexsiz = std::min(VertexMaxSiz, cur_vertexsiz + 1);
+        glBufferSubData(GL_ARRAY_BUFFER, 0, sizeof(Vertex)*cur_vertexsiz, vertices.data());
+    }
 }
 
 void mouse_button_callback(GLFWwindow *window, int button, int action, int mods) {
-    // action: GLFW_PRESS or GLFW_RELEASE
+        // button:  GLFW_MOUSE_BUTTON_1-8
+        // action:  GLFW_PRESS, GLFW_RELEASE
+        // mods:    GLFW_MOD_SHIFT, GLFW_MOD_CONTROL, GLFW_MOD_ALT ...
+
     if (button == GLFW_MOUSE_BUTTON_LEFT && action == GLFW_PRESS) {
-        std::cout << "PRESSED" << std::endl;
+        std::cout << "Dragging" << std::endl;
+        Dragging = true;
+    } else if (button == GLFW_MOUSE_BUTTON_LEFT && action == GLFW_RELEASE) {
+        std::cout << "Released" << std::endl;
+        Dragging = false;
+    } else if (button == GLFW_MOUSE_BUTTON_RIGHT && action == GLFW_PRESS) {
+        cur_vertexsiz = std::max(0, cur_vertexsiz - 1);
     }
 }
