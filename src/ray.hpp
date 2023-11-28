a0793e0677342911898ba7434095bd574835bb37
sakajo6 <sakajoy9@gmail.com>
2023-11-28 16:54:26 +0900

    Deleted checkSphere(), meshIntersection(), searchNeighborPoints()

--- src/ray.hpp
+++ src/ray.hpp
@@ -22,21 +22,6 @@ class Ray {
     Ray(double xScreen, double yScreen);  // Ray from { xScreen, yScreen }
     Ray(glm::dvec3 p, glm::dvec3 q);  // Ray from p to q
 
-    // Check whether this ray intersect
-    // with the sphere specified with 
-    // center and radius.
-    Hit checkSphere(glm::dvec3 center, double radius);
-
-    // Search for the nearest neighbor point
-    // along the specified line through mesh.
-    Hit meshIntersection(Eigen::MatrixXd &meshV, Eigen::MatrixXi &meshF, PointCloud *pointCloud);
-
-    // Search for the nearest neighbor points
-    // along the specified line through point cloud
-    // The search range is within the range
-    // of the searchRadius.
-    std::vector<Hit> searchNeighborPoints(double searchRadius, PointCloud *pointCloud);
-
     // Cast the point to the specified plane
     // that is parallel to "camera plane".
     Hit castPointToPlane(Plane* plane);
